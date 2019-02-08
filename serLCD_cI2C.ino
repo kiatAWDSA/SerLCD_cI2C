00fb2594e57ac98b3ad246c8a643ec91e737536b
kiatAWDSA <lskiat@huskers.unl.edu>
2019-02-08 17:45:52 -0600

    Add project files.

---
+++ serLCD_cI2C.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		serLCD_cI2C.ino
+ Created:	2/8/2019 5:22:40 PM
+ Author:	slau4
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <I2C.h>
+#include <SPI.h>
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
